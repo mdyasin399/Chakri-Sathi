c3179d58a58f99ed880fb0a5e52c3832c6bf5837
Md Yasin <83154902+mdyasin399@users.noreply.github.com>
2025-04-20 00:55:26 +0600

    Update job.G

--- Job-Grooming.php
+++ Job-Grooming.php
@@ -7,6 +7,70 @@
     <link rel="stylesheet" href="style0.css">
     <link rel="icon" href="img/logo.png">
 </head>
+<!-- Header Section -->
+    <header>
+        <div class="logo">
+            <img src="img/logo.png" alt="চাকরি সাথী Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="">Profile</a></li>
+                <li><a href="Job-Grooming.php">Job Grooming</a></li>
+                <li><a href="">Job Listing</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Contact Us</a></li>                
+            </ul>
+        </nav>
+        <div class="auth-buttons">
+            <a href="user_login.php"> <button class="login">Login</button></a>
+            <a href="user_register.php"> <button class="signup">Sign up</button></a>
+        </div>
+    </header>
+<!-- Header Section End -->
+
+<!-- Nav Section -->
+    <section class="nav">
+        <div class="search-bar">
+            <input type="text" placeholder="Keyword Search">
+            <input type="text" placeholder="Select Your Category">
+            <button>Search</button>
+        </div>
+        <div class="search-barr">
+            <button>Apply For A Job</button>
+            <button>Post A Job</button>
+        </div>
+    </section>
+<!-- Nav Section End-->
+
+<!-- Stats Section -->
+<section class="stats">
+    <div class="stat-item">
+        <img src="icon/LIVE JOBS.png" alt="img" width="50" height="50">
+        <p>LIVE JOBS</p>
+        <h2>3500+</h2>
+        
+    </div>
+    <div class="stat-item">
+        <img src="icon/COMPANIES.png" alt="img" width="50" height="50">
+        <p>COMPANIES</p>
+        <h2>250+</h2>
+        
+    </div>
+    <div class="stat-item">
+        <img src="icon/VACANCIES.png" alt="img" width="50" height="50">
+        <p>VACANCIES</p>
+        <h2>3000+</h2>
+        
+    </div>
+    <div class="stat-item">
+        <img src="icon/NEW JOBS.png" alt="img" width="50" height="50">
+        <p>NEW JOBS</p>
+        <h2>150+</h2>
+        
+    </div>
+</section>
+<!-- Stats Section End-->
 <body>
     
 </body>
