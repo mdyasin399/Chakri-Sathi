76fd3c3c6f51502ea54fec84c41d68ef65985e53
Md Yasin <83154902+mdyasin399@users.noreply.github.com>
2025-04-20 00:22:42 +0600

    Added new pages

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>চাকরি সাথী | Home</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style0.css">
     <link rel="icon" href="img/logo.png">
 </head>
 <body>
@@ -15,17 +15,17 @@
         </div>
         <nav>
             <ul>
-                <li><a href="">Home</a></li>
+                <li><a href="index.html">Home</a></li>
                 <li><a href="">Profile</a></li>
-                <li><a href="">Job Grooming</a></li>
+                <li><a href="Job-Grooming.php">Job Grooming</a></li>
                 <li><a href="">Job Listing</a></li>
                 <li><a href="">About Us</a></li>
                 <li><a href="">Contact Us</a></li>                
             </ul>
         </nav>
         <div class="auth-buttons">
-            <a href=""> <button class="login">Login</button></a>
-            <a href=""> <button class="signup">Sign up</button></a>
+            <a href="user_login.php"> <button class="login">Login</button></a>
+            <a href="user_register.php"> <button class="signup">Sign up</button></a>
         </div>
     </header>
 
