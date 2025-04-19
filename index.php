c82c6ff3ac2518c2e65c0ef8fdc871d874dcded0
Md Yasin <83154902+mdyasin399@users.noreply.github.com>
2025-04-19 13:27:23 +0600

    Update index

--- index.php
+++ index.php
@@ -3,9 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chakri Sathi</title>
+    <title>চাকরি সাথী | Home</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/logo.png">
 </head>
 <body>
-    
+
+    <header>
+        <div class="logo">
+            <img src="img/logo.png" alt="চাকরি সাথী Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Profile</a></li>
+                <li><a href="">Job Grooming</a></li>
+                <li><a href="">Job Listing</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Contact Us</a></li>                
+            </ul>
+        </nav>
+        <div class="auth-buttons">
+            <a href=""> <button class="login">Login</button></a>
+            <a href=""> <button class="signup">Sign up</button></a>
+        </div>
+    </header>
+
+
 </body>
 </html>
