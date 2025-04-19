bafcf0b098d03c88093fa72d14ad64d9805b36c1
Md Yasin <83154902+mdyasin399@users.noreply.github.com>
2025-04-20 00:53:04 +0600

    Update job grooming

--- Job-Grooming.php
+++ Job-Grooming.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>চাকরি সাথী | Job Grooming</title>
+    <link rel="stylesheet" href="style0.css">
+    <link rel="icon" href="img/logo.png">
 </head>
 <body>
     
